22943e69161a6a37fb48ba361b545e0d3c083d3c
goutham <goutham@gouthams-MacBook-Air-6.local>
2022-08-11 08:55:45 +0530

    modified

--- backend/post-function.ts
+++ backend/post-function.ts
@@ -1,34 +1,40 @@
 export {};
 const fs = require("fs");
 const timestamp = require("unix-timestamp");
-
+let array = [];
 const postFunction = (req: any, res: any) => {
-    
   if (!req.file.originalname.includes("txt")) {
     res.status(400).json({
       status: "error",
     });
   }
+
   const textFile = fs
     .readFileSync(`upload/${req.file.originalname}`)
     .toString();
-  const { transactionId } = JSON.parse(
-    textFile
-      .split("-")
-      .slice(4, textFile.length - 1)
-      .join("")
-  );
-
-  fs.writeFileSync(
-    `jsonfiles/${req.file.originalname}.json`,
-    JSON.stringify([
-      {
+  textFile.split("}").map((textFiles) => {
+    const data = textFiles + "}";
+    if (!(data === "}")) {
+      const { transactionId, details } = JSON.parse(
+        ` ${data
+          .split("-")
+          .slice(4, textFile.length - 1)
+          .join("")}
+       `
+      );
+      textFiles = textFiles.toString("");
+      array.push({
         timestamp: timestamp.now().toString(),
         loglevel: textFile.split("-")[3].trim(),
         transactionId: transactionId,
-        err: "not found",
-      },
-    ])
+        err: details,
+      });
+    }
+  });
+
+  fs.writeFileSync(
+    `jsonfiles/${req.file.originalname}.json`,
+    JSON.stringify(array)
   );
   res.download(`jsonfiles/${req.file.originalname}.json`);
 };
